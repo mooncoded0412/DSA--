9347bc7936c9ca0ca884f364f1891b84b47d6010
Arshia Muskaan <sk.arshia.muskaan@gmail.com>
2026-07-11 22:15:31 +0530

    LCS - Tabulation

--- DP/LCS.java
+++ DP/LCS.java
@@ -15,7 +15,7 @@ public class LCS{
     
     static int t[][];
 
-        //Memoization Approach
+        /*Memoization Approach
     public static int LCS(String[] x, String[] y, int m, int n){
         //Base case
         if(m == 0 || n == 0){
@@ -34,6 +34,23 @@ public class LCS{
         else{
             return t[m][n] = Math.max(LCS(x, y, m, n - 1), LCS(x, y, m - 1, n));
         }
+    }*/
+    //TOP - DOWN APPROACH
+    public static int LCS(String x[], String y[], int m, int n){
+        int[][] t = new int[m + 1][n + 1];
+
+        for(int i = 1; i <= m; i ++){
+            for(int j = 1; j <= n; j ++){
+                if(x[i - 1].equals(y[j - 1])){
+                    //if chars match, take diagonal + 1
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                }
+                else{
+                    t[i][j] = Math.max(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        return t[m][n];
     }
 
         public static void main(String args[]){
